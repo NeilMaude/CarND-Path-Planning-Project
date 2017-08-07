8cd4039d5fd8ffdea7380b0b556f5149d6abed60
NeilMaude <neil.maude@gmail.com>
2017-08-07 21:40:14 +0100

    Daily check-in

--- src/main.cpp
+++ src/main.cpp
@@ -240,6 +240,66 @@ int main() {
 
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+
+          	/*
+          	// Sample code to just send a straight line to the simulator
+          	double dist_inc = 0.5;
+            for(int i = 0; i < 50; i++)
+            {
+                  next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
+                  next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+            }*/
+
+/*
+            // Sample code to send the car in circles
+            double pos_x;
+            double pos_y;
+            double angle;
+            int path_size = previous_path_x.size();
+
+            for(int i = 0; i < path_size; i++)
+            {
+                next_x_vals.push_back(previous_path_x[i]);
+                next_y_vals.push_back(previous_path_y[i]);
+            }
+
+            if(path_size == 0)
+            {
+                pos_x = car_x;
+                pos_y = car_y;
+                angle = deg2rad(car_yaw);
+            }
+            else
+            {
+                pos_x = previous_path_x[path_size-1];
+                pos_y = previous_path_y[path_size-1];
+
+                double pos_x2 = previous_path_x[path_size-2];
+                double pos_y2 = previous_path_y[path_size-2];
+                angle = atan2(pos_y-pos_y2,pos_x-pos_x2);
+            }
+
+            double dist_inc = 0.5;
+            for(int i = 0; i < 50-path_size; i++)
+            {
+                next_x_vals.push_back(pos_x+(dist_inc)*cos(angle+(i+1)*(pi()/100)));
+                next_y_vals.push_back(pos_y+(dist_inc)*sin(angle+(i+1)*(pi()/100)));
+                pos_x += (dist_inc)*cos(angle+(i+1)*(pi()/100));
+                pos_y += (dist_inc)*sin(angle+(i+1)*(pi()/100));
+            }
+            // End of circles code
+*/
+
+          	// Can we just send the car along the current lane?
+          	double dist_inc = 5; //0.5;
+          	vector<double> new_xy;
+          	for(int i = 0; i < 50; i++)
+          	{
+          		new_xy = getXY(car_s + (dist_inc * i), car_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          		next_x_vals.push_back(new_xy[0]);
+          		next_y_vals.push_back(new_xy[1]);
+          	}
+
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
